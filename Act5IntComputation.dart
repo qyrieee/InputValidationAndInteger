7723b35b083af1902f01da1bdd7a434f2f47dd71
Princelyn Limen <princelynlimen06@gmail.com>
2025-08-11 12:54:09 +0800

    User Input Validation and Integer Computation

---
+++ Act5IntComputation.dart
@@ -0,0 +1,21 @@
+import 'dart:io';
+
+
+
+
+void main() {
+  print("Enter First number: ");
+  int? number1 = int.parse(stdin.readLineSync()!);
+
+  print("Enter Second number: ");
+  int? number2 = int.parse(stdin.readLineSync()!);
+
+  print("Enter Third number is: ");
+  int? number3 = int.parse(stdin.readLineSync()!);
+
+  print("The sum is ${number1 + number2 + number3}");
+  print("The diff is ${number1 - number2 - number3}");
+  print("The multi is ${number3 * number1 * number2}");
+  print("The quotient is ${number2 / number3 / number1}");
+  print("The percentage is ${number2 % number3}");
+}
